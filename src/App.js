f6a5cc0bde277a5979eda328327178ba559cec65
fanxiangling <xbackfan@didichuxing.com>
2021-12-15 00:12:14 +0800

    feat: jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      margin: 'auto',
+      width: 800,
+      paddingTop: '1rem'
+
+    }}>
+      <h1 className="title"> hello react</h1>
+      <table width="100%">
+        <thead>
+        <tr>
+            <th>name</th>
+            <th>age</th>
+            <th>address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>zyx</td>
+            <td>30</td>
+            <td>beijing</td>
+          </tr>
+        </tbody>
+      </table>
+      
     </div>
   );
 }
